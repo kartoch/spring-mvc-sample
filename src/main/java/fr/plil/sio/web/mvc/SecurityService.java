deb4674fc13071aa938eef9d5b0d62196c705168
jcartign <julien.cartigny@polytech-lille.fr>
2017-06-08 10:50:47 +0200

    Add SecurityService to get username of logged user

---
+++ src/main/java/fr/plil/sio/web/mvc/SecurityService.java
@@ -0,0 +1,6 @@
+package fr.plil.sio.web.mvc;
+
+public interface SecurityService {
+
+    String findLoggedInUsername();
+}
---
+++ src/main/java/fr/plil/sio/web/mvc/SecurityServiceImpl.java
@@ -0,0 +1,23 @@
+package fr.plil.sio.web.mvc;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService{
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    @Override
+    public String findLoggedInUsername() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getUsername();
+        }
+        return null;
+    }
+}
