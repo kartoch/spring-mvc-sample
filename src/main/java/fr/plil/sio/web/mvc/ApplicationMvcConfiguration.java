b273cfa3f07b48702784477afbe3cb61313e736d
jcartign <julien.cartigny@polytech-lille.fr>
2017-06-17 15:48:59 +0200

    moving password encoder to application security configuration

--- src/main/java/fr/plil/sio/web/mvc/ApplicationMvcConfiguration.java
+++ src/main/java/fr/plil/sio/web/mvc/ApplicationMvcConfiguration.java
@@ -2,16 +2,9 @@ package fr.plil.sio.web.mvc;
 
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootConfiguration
 public class ApplicationMvcConfiguration extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
--- src/main/java/fr/plil/sio/web/mvc/ApplicationSecurityConfiguration.java
+++ src/main/java/fr/plil/sio/web/mvc/ApplicationSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package fr.plil.sio.web.mvc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.Resource;
@@ -18,10 +20,12 @@ import javax.annotation.Resource;
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Resource
-    private PasswordEncoder passwordEncoder;
-
-    @Resource
     private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,6 +44,6 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
