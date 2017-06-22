0ba92d42b1a544b9d904356cf994758a27b819ff
jcartign <julien.cartigny@polytech-lille.fr>
2017-06-22 09:28:10 +0200

    No need for SpringBootServerInitializer

--- src/main/java/fr/plil/sio/web/mvc/Application.java
+++ src/main/java/fr/plil/sio/web/mvc/Application.java
@@ -2,11 +2,10 @@ package fr.plil.sio.web.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
